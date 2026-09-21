6ef7190e29c8f1b808fabaca51a7f810307d74ca
Sebas_Arias <sebastian.arias02@uptc.edu.co>
2026-09-21 17:06:12 -0500

    Primera version Ruta Segura Tunja

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_ESTATICO = "ruta-segura-static-v3";
+const CACHE_DINAMICO = "ruta-segura-dynamic-v3";
+const LIMITE_CACHE_DINAMICO = 20;
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./css/styles.css",
+  "./js/datos.js",
+  "./js/ui.js",
+  "./js/app.js",
+  "./img/icon.svg",
+];
